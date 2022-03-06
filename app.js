b3f0328f9488e5e144ef60afb8369e9b42264769
Bruno <bdelafontaine92@gmail.com>
2022-03-05 22:38:57 -0500

    Remove date and make use of mongoose db

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
 const mongoose = require("mongoose")
 
-
-mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB")
-const fruitSchema = new mongoose.Schema({
+async function test() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB")
+    const fruitSchema = new mongoose.Schema({
     name: {
         type: String,
         required :true
@@ -14,94 +14,102 @@ const fruitSchema = new mongoose.Schema({
         max: 10,
     },
     review: String
-});
+    });
 
-const personSchema = new mongoose.Schema({
+    const personSchema = new mongoose.Schema({
     name: String,
     age: Number,
     favoriteFruit: fruitSchema
-})
+    })
 
-const Person = mongoose.model("Person", personSchema);
-const Fruit = mongoose.model("Fruit", fruitSchema);
+    const Person = mongoose.model("Person", personSchema);
+    const Fruit = mongoose.model("Fruit", fruitSchema);
 
-const fruit_pine = new Fruit( {
+    const fruit_pine = new Fruit( {
     name: "pineapple",
     rating: 9,
     review:" decent goodsz"
-})
+    })
 
-const raspberry = new Fruit( {
+    const raspberry = new Fruit( {
     name: "raspberry",
     rating: 10,
     review:" da best"
-})
+    })
 
-raspberry.save();
+    // raspberry.save();
 
-// const person  = new Person({
-//     name: "amy",
-//     age: 12,
-//     favoriteFruit: fruit_pine
-// })
-// person.save();
-// person.save();
-
-
-// const fruit = new Fruit( {
-//     name: "Peach",
-//     rating: 10,
-//     review:" goodsz"
-// })
+    // const person  = new Person({
+    //     name: "amy",
+    //     age: 12,
+    //     favoriteFruit: fruit_pine
+    // })
+    // person.save();
+    // person.save();
 
 
-
-// fruit.save(function(err){
-//     console.log(err);
-//     // mongoose.connection.close(function(err) {
-//     //     console.log("save success");
-//     //     process.exit(0)
-//     // })
-// })
-
-// Fruit.find(function(err, fruits) {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         fruits.forEach(element => {
-//             console.log(element.name);
-//         });
-//         mongoose.connection.close(function(err) {
-//             process.exit(0)
-//         })
-//         mongoose.connection.close(function () {
-//             console.log('Mongoose disconnected on app termination');
-//             process.exit(0);
-//           });
-//     }
-// })
+    // const fruit = new Fruit( {
+    //     name: "Peach",
+    //     rating: 10,
+    //     review:" goodsz"
+    // })
 
 
-Person.updateOne({name:"John"}, {favoriteFruit: raspberry}, function(err) {
-    if (err)
-    {
-        console.log(err);
-    } else {
-        console.log("succes");
-    }
-});
 
-Fruit.deleteOne({name: "Peach"}, function (err) {
+    // fruit.save(function(err){
+    //     console.log(err);
+    //     // mongoose.connection.close(function(err) {
+    //     //     console.log("save success");
+    //     //     process.exit(0)
+    //     // })
+    // })
+
+    const x = await Fruit.find(function(err, fruits) {
     if (err) {
         console.log(err);
     } else {
-        console.log("Success delete");
-        mongoose.connection.close(function () {
-            console.log('Mongoose disconnected on app termination');
-            process.exit(0);
+        fruits.forEach(element => {
+            console.log(element.name);
         });
+        // mongoose.connection.close(function(err) {
+        //     process.exit(0)
+        // })
+        // mongoose.connection.close(function () {
+        //     console.log('Mongoose disconnected on app termination');
+        //     process.exit(0);
+        //   });
     }
-})
+    })
+
+    mongoose.connection.close();
+}
+test()
+
+// Person.updateOne({name:"John"}, {favoriteFruit: raspberry}, function(err) {
+//     if (err)
+//     {
+//         console.log(err);
+//     } else {
+//         console.log("succes");
+//     }
+// });
+
+// Fruit.deleteOne({name: "Peach"}, function (err) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Success delete");
+//         mongoose.connection.close(function () {
+//             console.log('Mongoose disconnected on app termination');
+//             process.exit(0);
+//         });
+//     }
+// })
+// async function close() {
+//     await mongoose.connection.close()
+// }
+// close()
+
 
   const findDocuments = function(db, callback) {
     // Get the documents collection
